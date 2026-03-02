7b472ad46acbc2552ebc2cdab8ef6c76cb566827
Aidan Weeks <20106513@wit.ie>
2026-03-02 00:52:02 +0000

    initial commit for textfield, button and color xml file,

--- app/src/main/java/ie/setu/basketball_club_application/PlayerList.kt
+++ app/src/main/java/ie/setu/basketball_club_application/PlayerList.kt
@@ -5,16 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
+import timber.log.Timber.i
 
-class MainActivity : AppCompatActivity() {
+class PlayerList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.player_list)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Timber.plant(Timber.DebugTree())
+        i("Placemark Activity started...")
     }
 }
